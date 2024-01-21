5c5117ef822915358e2d8b4972cef2dc5b97b920
Marian Kostadinov <manchokapitancho@gmail.com>
2024-01-21 18:03:02 +0100

    Added start transaction

--- src/TransactionContext.php
+++ src/TransactionContext.php
@@ -3,6 +3,7 @@
 namespace Walnut\Lib\TransactionContext;
 
 interface TransactionContext {
+	public function startTransaction(): void;
 	public function saveChanges(): void;
 	public function revertChanges(): void;
 }
